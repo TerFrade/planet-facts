9a8d0234cb2a1cdae34cf13e779e2fe621e12da3
TerFrade <TerenceFrade@gmail.com>
2022-07-26 22:51:33 +0200

    finished most of the desktop styling for the navbar

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,9 +1,13 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
+  font-size: 2rem;
+  font-weight: 600;
   padding: 2rem 3.2rem;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+
   > * {
     flex-basis: 100%;
   }
@@ -15,14 +19,18 @@ export const Title = styled.div`
 
 export const Nav = styled.div`
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
+
   a {
-    padding: 0.58rem 1rem;
-    border: 1px solid black;
-    color: black;
-    font-size: 0.9375rem;
-    text-transform: uppercase;
-    text-decoration: none;
+    padding: 0.25rem 0.75rem;
+    letter-spacing: 0.1rem;
+    font-size: 1.25rem;
     font-weight: 600;
+    text-decoration: none;
+    text-transform: uppercase;
+    :hover {
+      background-color: #6518ca;
+    }
   }
 `
--- src/components/RouteOutlet.tsx
+++ src/components/RouteOutlet.tsx
@@ -6,11 +6,13 @@ import Planet, { Props } from "../pages/Planet"
 
 const RouteOutlet: React.FC<Props> = ({ planets }: Props) => {
   return (
-    <Routes>
-      <Route path="/*" element={<Navigate to="/planet/earth" />} />
-      <Route path="/planet/:name" element={<Planet planets={planets} />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <div className="main-content">
+      <Routes>
+        <Route path="/*" element={<Navigate to="/planet/earth" />} />
+        <Route path="/planet/:name" element={<Planet planets={planets} />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
   )
 }
 
