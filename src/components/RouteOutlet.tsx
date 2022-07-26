789d56b37a945823c600a1bded2f7d9c2d85aaed
TerFrade <TerenceFrade@gmail.com>
2022-07-26 23:50:46 +0200

    fixed routing to match deployed url

--- src/components/RouteOutlet.tsx
+++ src/components/RouteOutlet.tsx
@@ -8,8 +8,11 @@ const RouteOutlet: React.FC<Props> = ({ planets }: Props) => {
   return (
     <div className="main-content">
       <Routes>
-        <Route path="/*" element={<Navigate to="/planet/earth" />} />
-        <Route path="/planet/:name" element={<Planet planets={planets} />} />
+        <Route path="/*" element={<Navigate to="/planet-facts/earth" />} />
+        <Route
+          path="/planet-facts/:name"
+          element={<Planet planets={planets} />}
+        />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
