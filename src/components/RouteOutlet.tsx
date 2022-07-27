f335e4597aa42d2b0d2db97d718cf41eacfbb8e9
TerFrade <TerenceFrade@gmail.com>
2022-07-27 21:16:06 +0200

    minor route fixing

--- src/components/RouteOutlet.tsx
+++ src/components/RouteOutlet.tsx
@@ -8,11 +8,8 @@ const RouteOutlet: React.FC<Props> = ({ planets }: Props) => {
   return (
     <div className="main-content">
       <Routes>
-        <Route path="/*" element={<Navigate to="/planet-facts/earth" />} />
-        <Route
-          path="/planet-facts/:name"
-          element={<Planet planets={planets} />}
-        />
+        <Route path="/" element={<Navigate to="/earth" />} />
+        <Route path="/:name" element={<Planet planets={planets} />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
